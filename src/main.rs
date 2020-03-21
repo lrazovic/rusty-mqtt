cb1d3a1e7c75e28a009e87447252991a455f3ba4
Leonardo Razovic <leorazzo@gmail.com>
2020-03-21 18:22:04 +0100

    Add serde in order to seriale a struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct MeteoStation {
+    temperature: f32,
+    humidity: i16,
+    rain_height: i16,
+    wind_direction: i16,
+    wind_intensity: i16,
+}
+
+impl MeteoStation {
+    fn new(
+        temperature: f32,
+        humidity: i16,
+        rain_height: i16,
+        wind_direction: i16,
+        wind_intensity: i16,
+    ) -> Self {
+        Self {
+            temperature,
+            humidity,
+            rain_height,
+            wind_direction,
+            wind_intensity,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, World!")
+    let device = MeteoStation::new(1., 2, 3, 4, 5);
 }
