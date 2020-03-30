3033e12c3a7fb14aa58b569ab8e0f76a8a625023
Leonardo Razovic <leorazzo@gmail.com>
2020-03-30 12:25:36 +0200

    simple version of MQTT-SN/MQTT transparent bridge

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 use clap::{App, Arg};
-use log::info;
+use log::{error, info, trace};
 use mqtt::control::variable_header::ConnectReturnCode;
-use mqtt::packet::{ConnackPacket, ConnectPacket};
+use mqtt::packet::*;
+use mqtt::TopicFilter;
 use mqtt::TopicName;
-use mqtt::{Decodable, Encodable};
-use rand::prelude::thread_rng;
-use std::collections::HashMap;
+use mqtt::{Decodable, Encodable, QualityOfService};
 use std::env;
 use std::io::Write;
-use std::net::TcpStream;
-use std::thread;
+use std::net;
+use std::net::{IpAddr, Ipv6Addr, SocketAddr};
+use std::str;
 use std::time::Duration;
 
+use futures::join;
+use futures::prelude::*;
+use tokio::net::TcpStream;
+use tokio::prelude::*;
 mod utils;
-fn main() {
+
+#[tokio::main]
+async fn main() {
     // Loger Initialization
     env::set_var(
         "RUST_LOG",
@@ -38,6 +44,7 @@ fn main() {
             Arg::with_name("USER_NAME")
                 .short("u")
                 .long("username")
+                .default_value("dyGTnPixbX1WiOsP92DT")
                 .required(true)
                 .takes_value(true)
                 .help("Login user name"),
@@ -60,36 +67,53 @@ fn main() {
         )
         .get_matches();
     let server_addr = matches.value_of("SERVER").unwrap();
-    let server_port = matches.value_of("PORT").unwrap();
+    let server_port: u16 = matches.value_of("PORT").unwrap().parse().unwrap();
     let host = format!("{}:{}", server_addr, server_port);
     let client_id = matches
         .value_of("CLIENT_ID")
         .map(|x| x.to_owned())
         .unwrap_or_else(utils::generate_client_id);
     let user_name = matches.value_of("USER_NAME").map(|x| x.to_owned()).unwrap();
-    let devices_number: i32 = matches
-        .value_of("NUMBER")
-        .map(|x| x.to_owned())
-        .unwrap()
-        .parse()
-        .unwrap();
 
     info!("Connecting to {:?} ... ", host);
     info!("Client identifier {:?}", client_id);
+    let ipv6_addr = IpAddr::V6(Ipv6Addr::LOCALHOST);
+    let socket_addr = SocketAddr::new(ipv6_addr, 1888);
 
-    //Opens a TCP connection to a remote host.
-    let mut stream = TcpStream::connect(host.clone()).unwrap();
+    //Opens a TCP connection to RSMB.
+    let mut rsmb_stream = net::TcpStream::connect(socket_addr).unwrap();
 
-    // Create and Send an initial MQTT CONNECT packet.
+    //Opens a TCP connection to ThingsBoard.
+    let mut thingsboard_stream = net::TcpStream::connect(&host).unwrap();
+
+    // Create and Send an initial MQTT CONNECT packet to Thingsboard.
+    let mut conn = ConnectPacket::new("MQTT", client_id.clone());
+    conn.set_clean_session(true);
+    conn.set_keep_alive(10);
+    let mut buf = Vec::new();
+    conn.encode(&mut buf).unwrap();
+    rsmb_stream.write_all(&buf[..]).unwrap();
+
+    let connack = ConnackPacket::decode(&mut rsmb_stream).unwrap();
+    trace!("CONNACK {:?}", connack);
+
+    if connack.connect_return_code() != ConnectReturnCode::ConnectionAccepted {
+        panic!(
+            "Failed to connect to server, return code {:?}",
+            connack.connect_return_code()
+        );
+    }
+
+    // Create and Send an initial MQTT CONNECT packet to Thingsboard.
     let mut conn = ConnectPacket::new("MQTT", client_id);
     conn.set_clean_session(true);
     conn.set_user_name(Some(user_name));
     let mut buf = Vec::new();
     conn.encode(&mut buf).unwrap();
-    stream.write_all(&buf[..]).unwrap();
+    thingsboard_stream.write_all(&buf[..]).unwrap();
 
     // Check if the connection is accepted
-    let connack = ConnackPacket::decode(&mut stream).unwrap();
+    let connack = ConnackPacket::decode(&mut thingsboard_stream).unwrap();
     if connack.connect_return_code() != ConnectReturnCode::ConnectionAccepted {
         panic!(
             "Failed to connect to server, return code {:?}",
@@ -98,37 +122,79 @@ fn main() {
     }
     info!("Successfully connected to {:?}", host);
 
-    //Connect Gateway and Devices
-    let connection_topic = TopicName::new("v1/gateway/connect").unwrap();
-    for i in 0..devices_number {
-        let device_name = format!("station_{}", i);
-        let device = utils::Device::new(device_name);
-        let message = serde_json::to_string(&device).unwrap();
-        utils::publish(&mut stream, message, connection_topic.clone());
-        info!("Gateway and Device {} connected!", i);
+    let mut channel_filters: Vec<(TopicFilter, QualityOfService)> = Vec::new();
+    channel_filters.push((
+        TopicFilter::new("hello/world").unwrap(),
+        QualityOfService::Level0,
+    ));
+    let sub = SubscribePacket::new(10, channel_filters);
+    let mut buf = Vec::new();
+    sub.encode(&mut buf).unwrap();
+    rsmb_stream.write_all(&buf[..]).unwrap();
+
+    loop {
+        let packet = match VariablePacket::decode(&mut rsmb_stream) {
+            Ok(pk) => pk,
+            Err(err) => {
+                error!("Error in receiving packet {:?}", err);
+                continue;
+            }
+        };
+        trace!("PACKET {:?}", packet);
+
+        if let VariablePacket::SubackPacket(ref ack) = packet {
+            if ack.packet_identifier() != 10 {
+                panic!("SUBACK packet identifier not match");
+            }
+
+            info!("Subscribed!");
+            break;
+        }
     }
 
-    // Create and publish random data on the given Topic
-    let rng = thread_rng();
-    let telemtry_topic = TopicName::new("v1/gateway/telemetry").unwrap();
-    let attribute_topic = TopicName::new("v1/gateway/attributes").unwrap();
-    let mut telemetry = HashMap::new();
-    let mut attributes = HashMap::new();
-    loop {
-        for i in 0..devices_number {
-            let mut vector_values = Vec::new();
-            let key = format!("station_{}", i);
-            let values = utils::generate_packet(rng);
-            let sensor_telemetry = utils::generate_telemtry_packet(&values);
-            let sensor_attributes = utils::generate_attribute_packet(&values);
-            vector_values.push(sensor_telemetry);
-            telemetry.insert(key.clone(), vector_values);
-            attributes.insert(key, sensor_attributes);
+    // connection made, start the async work
+    let mut stream = TcpStream::from_std(rsmb_stream).unwrap();
+    let (mut mqtt_read, mut mqtt_write) = stream.split();
+
+    let ping_time = Duration::new((10 / 2) as u64, 0);
+    let mut ping_stream = tokio::time::interval(ping_time);
+
+    let ping_sender = async move {
+        while let Some(_) = ping_stream.next().await {
+            info!("Sending PINGREQ to broker");
+
+            let pingreq_packet = PingreqPacket::new();
+
+            let mut buf = Vec::new();
+            pingreq_packet.encode(&mut buf).unwrap();
+            mqtt_write.write_all(&buf).await.unwrap();
         }
-        let serialized_telemetry = serde_json::to_string(&telemetry).unwrap();
-        let serialized_attributes = serde_json::to_string(&attributes).unwrap();
-        utils::publish(&mut stream, serialized_telemetry, telemtry_topic.clone());
-        utils::publish(&mut stream, serialized_attributes, attribute_topic.clone());
-        thread::sleep(Duration::from_secs(5))
-    }
+    };
+    let receiver = async move {
+        while let Ok(packet) = VariablePacket::parse(&mut mqtt_read).await {
+            trace!("PACKET {:?}", packet);
+
+            match packet {
+                VariablePacket::PingrespPacket(..) => {
+                    info!("Receiving PINGRESP from broker ..");
+                }
+                VariablePacket::PublishPacket(ref publ) => {
+                    let msg = match str::from_utf8(&publ.payload_ref()[..]) {
+                        Ok(msg) => msg,
+                        Err(err) => {
+                            error!("Failed to decode publish message {:?}", err);
+                            continue;
+                        }
+                    };
+                    info!("RECV on Topic ({}): {}", publ.topic_name(), msg);
+                    let telemtry_topic = TopicName::new("v1/devices/me/telemetry").unwrap();
+                    let value = format!("{{\"temperature\": \"{}\"}}", msg);
+                    utils::publish(&mut thingsboard_stream, value, telemtry_topic.clone());
+                }
+                _ => {}
+            }
+        }
+    };
+
+    join!(ping_sender, receiver);
 }
